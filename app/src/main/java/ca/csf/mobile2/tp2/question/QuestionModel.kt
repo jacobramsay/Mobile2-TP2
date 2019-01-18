104431c2ce4786588cc88e4c654726b6830564c9
Jacob Ramsay <ramsayjacob@gmail.com>
2019-01-18 10:49:24 -0500

    Cours 1

---
+++ app/src/main/java/ca/csf/mobile2/tp2/question/QuestionModel.kt
@@ -0,0 +1,7 @@
+package ca.csf.mobile2.tp2.question
+
+import java.util.*
+
+class QuestionModel(id: UUID, text: String, choice1: String, choice2: String, nbChoice1: Int, nbChoice2: Int) {
+
+}
---
+++ app/src/main/java/ca/csf/mobile2/tp2/question/QuestionService.kt
@@ -0,0 +1,48 @@
+package ca.csf.mobile2.tp2.question
+
+import org.androidannotations.annotations.Background
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.jackson.JacksonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.POST
+import java.io.IOException
+
+class QuestionService {
+
+    private val service : Service
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://m2t2.csfpwmjv.tk/")
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build()
+
+        service = retrofit.create(Service::class.java)
+    }
+
+    @Background
+    fun getRandomQuestion(onSuccess: (QuestionModel) -> Unit,
+                          onConnectivityError: () -> Unit,
+                          onServerError: () -> Unit){
+        try {
+            val response = service.getRandomQuestion().execute()
+            if(response.isSuccessful){
+                onSuccess(response.body()!!)
+            }else{
+                onServerError()
+            }
+        }
+        catch (e: IOException){
+            onConnectivityError()
+        }
+    }
+
+    private interface Service {
+        @GET("api/v1/question/random")
+        fun getRandomQuestion(): Call<QuestionModel>
+
+        //@POST
+    }
+}
